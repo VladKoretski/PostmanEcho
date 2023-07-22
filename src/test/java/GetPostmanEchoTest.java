c8555556dd3ead0498217e40da26ee1f33a8ce27
Vladko1973 <vpk1973@yandex.ru>
2023-07-22 19:54:56 +0300

    first commit

---
+++ src/test/java/GetPostmanEchoTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class GetPostmanEchoTest {
+
+    @Test
+    void shouldReturnSomething() {
+
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("hi guys") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("hi guys"))
+        ;
+    }
+}
