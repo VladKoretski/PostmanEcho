f355d41dec5615c0b98ac71e6e38be52d94e6db5
Vladko1973 <vpk1973@yandex.ru>
2023-08-05 09:25:17 +0300

    faillast

--- src/test/java/GetPostmanEchoTest.java
+++ src/test/java/GetPostmanEchoTest.java
@@ -8,18 +8,14 @@ public class GetPostmanEchoTest {
     @Test
     void shouldReturnSomething() {
 
-        // Given - When - Then
-// Предусловия
         given()
                 .baseUri("https://postman-echo.com")
-                .body("hi guys") // отправляемые данные (заголовки и query можно выставлять аналогично)
-// Выполняемые действия
+                .body("hi guys")
                 .when()
                 .post("/post")
-// Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("hi guys"))
+                .body("data", equalTo("hi guys!"))
         ;
     }
 }
